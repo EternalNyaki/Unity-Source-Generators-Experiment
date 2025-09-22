0db7d40aeb697185e6c3803bba7434c0e460616d
EternalNyaki <124942024+EternalNyaki@users.noreply.github.com>
2025-09-22 11:53:26 -0400

    UI Functionality

---
+++ Source Generator Experiment/Assets/Scripts/CharacterUIManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class CharacterUIManager : MonoBehaviour
+{
+    public Character character;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        character = new Character();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnNameChanged(string value)
+    {
+        character.name = value;
+    }
+
+    public void OnHealthChanged(string value)
+    {
+        int n;
+        if (System.Int32.TryParse(value, out n))
+        {
+            character.health = n;
+        }
+    }
+
+    public void OnStrengthChanged(string value)
+    {
+        int n;
+        if (System.Int32.TryParse(value, out n))
+        {
+            character.strength = n;
+        }
+    }
+
+    public void OnIntelligenceChanged(string value)
+    {
+        int n;
+        if (System.Int32.TryParse(value, out n))
+        {
+            character.intelligence = n;
+        }
+    }
+
+    public void OnDexterityChanged(string value)
+    {
+        int n;
+        if (System.Int32.TryParse(value, out n))
+        {
+            character.dexterity = n;
+        }
+    }
+}
